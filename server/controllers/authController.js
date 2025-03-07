77e7dceb277f6fc0ca9ba7acc153f3e6b7fe6761
Rajesh97266 <rajesh97266@gmail.com>
2025-03-07 23:38:38 +0530

    mail-option-created

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import userModel from "../models/userModel.js";
+// import { transporter } from "../config/nodemailer.js";
+import transporter from "../config/nodemailer.js"
 
 //register
 export const register = async (req, res) => {
@@ -32,6 +34,16 @@ export const register = async (req, res) => {
       sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
+    //send welcome email
+    const mailOptions = {
+    from: process.env.SENDER_EMAIL,
+      to: email,
+      subject: "Welcome to MERN Auth",
+      text: `Hello ${name}, welcome to MERN Auth. Your account has been created successfully.`,
+    };
+
+    await transporter.sendMail(mailOptions);
 
     return res.json({ success: true, message: "User registered successfully" });
   } catch (error) {
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { logout, register } from "../controllers/authController.js";
-import { login } from "../controllers/authController.js";
+import { logout, register , login } from "../controllers/authController.js";
 
-const router = express.Router();
+
+const authRouter = express.Router();
 
 // Register Route
-router.post("/register", register);
-router.post("/login", login);
-router.post("/logout", logout);
+authRouter.post("/register", register);
+authRouter.post("/login", login);
+authRouter.post("/logout", logout);
 
-export default router;
+export default authRouter;
