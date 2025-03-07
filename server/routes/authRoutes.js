0594789a579ca2344cda4b2f318df930e3a04974
Rajesh97266 <rajesh97266@gmail.com>
2025-03-07 22:04:59 +0530

    logout-functionality

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,12 +1,12 @@
 import express from "express";
-import { register } from "../controllers/authController.js";
+import { logout, register } from "../controllers/authController.js";
 import { login } from "../controllers/authController.js";
-
 
 const router = express.Router();
 
 // Register Route
 router.post("/register", register);
 router.post("/login", login);
+router.post("/logout", logout);
 
 export default router;
