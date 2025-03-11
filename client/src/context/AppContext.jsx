6390f3a7d7f6b25c36947384ebf963ccdfae0198
Rajesh97266 <rajesh97266@gmail.com>
2025-03-11 22:49:45 +0530

    vercel-host

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -13,9 +13,7 @@ export const AppContextProvider = (props) => {
 
   const getAuthState = async () => {
     try {
-      const { data } = await axios.get(
-        `${backendUrl.replace(/\/$/, "")}/api/auth/is-auth`
-      );
+      const { data } = await axios.get(backendUrl + "/api/auth/is-auth");
       if (data.success) {
         setIsLoggedin(true);
         getUserData();
@@ -27,9 +25,7 @@ export const AppContextProvider = (props) => {
 
   const getUserData = async () => {
     try {
-      const { data } = await axios.get(
-        `${backendUrl.replace(/\/$/, "")}/api/user/data`
-      );
+      const { data } = await axios.get(backendUrl + "/api/user/data");
       data.success ? setUserData(data.userData) : toast.error(data.message);
     } catch (error) {
       toast.error(error.message);
